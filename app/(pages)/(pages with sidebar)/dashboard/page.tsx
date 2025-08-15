2461eee19a0f9303ef0230fcd66694e191e1d338
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2025-08-15 19:23:40 +0200

    .

--- app/(pages)/(pages with sidebar)/dashboard/page.tsx
+++ app/(pages)/(pages with sidebar)/dashboard/page.tsx
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
 import QuizList from "@/app/components/QuizList";
 
 export default async function Dashboard() {
   return (
     <div className="">
       <QuizList />
-=======
-import SearchBar from "@/app/components/Search";
-import QuizList from "@/app/components/QuizList";
-
-interface DashboardProps {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}
-
-export default async function Dashboard({ searchParams }: DashboardProps) {
-  const query = (await searchParams).q as string;
-
-  return (
-    <div className="p-10">
-      <SearchBar />
-      <QuizList query={query} />
->>>>>>> 24648072ef600ff4a9b733abb77ba335ed3c5e0f
     </div>
   );
 }
